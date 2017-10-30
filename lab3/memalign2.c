0a94d215cbbe77986fe6c5da9b59ec40aa0c1749
student <student@localhost.localdomain>
2017-10-30 00:04:35 -0600

    Lab 3 finished

--- lab3/memalign2.c
+++ lab3/memalign2.c
@@ -4,18 +4,18 @@
 
 int main(){
     void *p1 = memalign2(32, 101);
-    printf("memalign2(32, 101)=> %d/32 = %.2f, \n", p1, (double)((int)p1/32));
+    printf("memalign2(32, 101)=> %d/32 = %.2f, \n", p1, (double)((int)p1/32.0));
     p1 = memalign2(32, 200);
-    printf("memalign2(32, 200)=> %d/32 = %.2f, \n", p1, (double)((int)p1/32));
+    printf("memalign2(32, 200)=> %d/32 = %.2f, \n", p1, (double)((int)p1/32.0));
     p1 = memalign2(32, 380);
-    printf("memalign2(32, 380)=> %d/32 = %.2f, \n\n", p1, (double)((int)p1/32));
+    printf("memalign2(32, 380)=> %d/32 = %.2f, \n\n", p1, (double)((int)p1/32.0));
     
     p1 = memalign2(64, 101);
-    printf("memalign2(64, 101)=> %d/64 = %.2f, \n", p1, (double)((int)p1/64));
+    printf("memalign2(64, 101)=> %d/64 = %.2f, \n", p1, (double)((int)p1/64.0));
     p1 = memalign2(64, 200);
-    printf("memalign2(64, 200)=> %d/64 = %.2f, \n", p1, (double)((int)p1/64));
+    printf("memalign2(64, 200)=> %d/64 = %.2f, \n", p1, (double)((int)p1/64.0));
     p1 = memalign2(64, 380);
-    printf("memalign2(64, 380)=> %d/64 = %.2f, \n\n", p1, (double)((int)p1/64));
+    printf("memalign2(64, 380)=> %d/64 = %.2f, \n\n", p1, (double)((int)p1/64.0));
     arenaCheck();
     return 0;
     
